20d96b7665bcfd2d6b58c7a85fabc77715422e41
PAULO <paulomartinez1999@gmail.com>
2020-11-17 18:07:34 -0500

    Corregido errores de lineas de codigo

--- Opreracion.java
+++ Opreracion.java
@@ -1,14 +1,14 @@
 public class Operacion{
-<<<<<<< HEAD
+
     
     public int resta( int a, int b ){
        int res = a-b;
        return res;
 
-=======
+
 
     public int resta( int a, int b ){
         return a-b;
->>>>>>> origin/Develop
+
     }
 }
